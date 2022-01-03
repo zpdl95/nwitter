2b5a14f8619e9da55a2a3429931abcac48927a9d
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-03 21:41:28 +0900

    #1.1 Securing the Keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 import firebase from "firebase/compat/app";
+
+/* 환경설정값을 사용하는 이유는 github때문이지 빌드할때 숨기기 위함이 아님. 빌드하면 다 값이 원래값으로 바뀜 */
 const firebaseConfig = {
-  apiKey: "AIzaSyAoHyvF7ZdmCUSXN44O9wr9cn33Tf2Iqn0",
-  authDomain: "nwitter-dc82e.firebaseapp.com",
-  projectId: "nwitter-dc82e",
-  storageBucket: "nwitter-dc82e.appspot.com",
-  messagingSenderId: "481321808280",
-  appId: "1:481321808280:web:1b79cdf6cda5c48f47fbed",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 export const fireBase = firebase.initializeApp(firebaseConfig);
