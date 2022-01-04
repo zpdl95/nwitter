688120be673a16c945a0e78a9fdb08296495f7b8
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-04 19:47:23 +0900

    #2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
 
   const onChange = (event) => {
     const {
@@ -14,8 +16,29 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        /* firebase의 유저생성코드 */
+        /* 유저생성을 해도 자동로그인 시켜줌 */
+        /* 로그인유지의 단계가 ['local','session','none']이 있다 */
+        /* local: 브라우저가 켜져있으면 유지
+            session: 탭이 켜져있으면 유지
+            none: 유지 안 함 */
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        /* firebase의 로그인코드 */
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
@@ -36,7 +59,10 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type={"submit"} placeholder="Log In" />
+        <input
+          type={"submit"}
+          value={newAccount ? "Create Account" : "Log In"}
+        />
       </form>
       <div>
         <button>Countinue with Google</button>
