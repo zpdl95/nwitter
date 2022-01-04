2ce7d0117b8e1abd7a371dbd3ba08f3dde9482db
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-05 00:09:13 +0900

    #2.6 Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,36 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {/* && = 단축평가 */}
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
         )}
       </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,20 @@
 import React from "react";
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  /* 기록 객체를 사용할 수 있는 hook */
+  const history = useHistory();
+  const onLogOutClick = () => {
+    /* firebase의 로그아웃 */
+    authService.signOut();
+    /* home으로 보내는 방법 */
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Sing Out</button>
+    </>
+  );
+};
 export default Profile;
