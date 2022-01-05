2e0433fe872b70386ec0fcf363fe67267f6a3db6
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-05 18:27:00 +0900

    #3.0 Form and Database Setup

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import "firebase/compat/database";
 
 /* 환경설정값을 사용하는 이유는 github때문이지 빌드할때 숨기기 위함이 아님. 빌드하면 다 값이 원래값으로 바뀜 */
 const firebaseConfig = {
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,31 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={nweet}
+          type={"text"}
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type={"submit"} value={"Nweet"} />
+      </form>
+    </div>
+  );
+};
 export default Home;
