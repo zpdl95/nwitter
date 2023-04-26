50640a125ee8204f6343b0267b9a0658e2ea3f17
zpdl95 <won8070@naver.com>
2023-04-26 21:56:59 +0900

    update for deploy

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,38 +1,37 @@
-import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
-import Navigation from "components/Navigation";
-import Profile from "routes/Profile";
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
+import Navigation from 'components/Navigation';
+import Profile from 'routes/Profile';
 
 const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
   return (
-    <Router basename="/">
-      {/* && = 단축평가 */}
+    <Router>
       {isLoggedIn && <Navigation userObj={userObj} />}
       <div
         style={{
           maxWidth: 890,
-          width: "100%",
-          margin: "0 auto",
+          width: '100%',
+          margin: '0 auto',
           marginTop: 80,
-          display: "flex",
-          justifyContent: "center",
+          display: 'flex',
+          justifyContent: 'center',
         }}
       >
         <Switch>
           {isLoggedIn ? (
             <>
-              <Route exact path="/">
+              <Route exact path='/'>
                 <Home userObj={userObj} />
               </Route>
-              <Route exact path="/profile">
+              <Route exact path='/profile'>
                 <Profile userObj={userObj} refreshUser={refreshUser} />
               </Route>
             </>
           ) : (
             <>
-              <Route exact path="/">
+              <Route exact path='/'>
                 <Auth />
               </Route>
             </>
